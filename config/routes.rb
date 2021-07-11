17a810574d5eb3d823c365570548d0a1adb354fd
stogashi146 <stogashi146@gmail.com>
2021-07-11 13:18:37 +0000

    [Finish] App-Challenge8d

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # resources :users,only: [:show,:index]
+  root to: 'homes#top'
+  devise_for :users
+  get 'home/about' => 'homes#about'
+  resources :books do
+    resource :favorites,only:[:create,:destroy]
+    resources :post_comments,only:[:create,:destroy]
+  end
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get "followings" => "relationships#followings",as:"followings"
+    get "followers" => "relationships#followers",as:"followers"
+  end
+  get "search" => "searches#search"
+
+
+end
